54701279f66d9bf7658da455c9ef82b33d015758
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-19 19:48:05 +0000

    To remove element from source

---
+++ without.js
@@ -0,0 +1,48 @@
+//Helper Function that compare the values of two arrays
+const eqArrays = function (array1, array2) {
+  let flagFalse = 0;
+  if (array1.length !== array2.length) {
+    flagFalse = 1;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        flagFalse = 1;
+      }
+    }
+    if (flagFalse === 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+const without = function (source, itemToRemove) {
+  let itemRemoveArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemToRemove[i]) {
+      itemRemoveArr.push(source[i]);
+    }
+  }
+  return itemRemoveArr;
+};
+
+//Function to check array using helper funtion eqArrays and log the message with input array values
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`No Items found to remove as [${array1}] === [${array2}]`);
+  } else {
+    console.log(`Items to be removed from source are: [${without(array1, array2)}]`);
+  }
+};
+
+//Test Run
+assertArraysEqual([1, 2, 3], [1]);
+assertArraysEqual(["1", "2", "3",], [1, 2, "3"]);
+assertArraysEqual(["1", "2", "3",], []);
+assertArraysEqual([], [1, 2, "3"]);
+
+//To check whether input Array remains same
+const words = ["hello", "world", "lighthouse"];
+(without(words, ["lighthouse"]));
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
