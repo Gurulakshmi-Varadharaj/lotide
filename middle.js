45e896ac875bbea1baf0cbb06903e5c68d7a6962
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-25 03:52:20 +0000

    Refactored using module and seperate program to test

--- middle.js
+++ middle.js
@@ -1,44 +1,3 @@
-//Helper Function that compare the values of two arrays
-const eqArrays = function (array1, array2) {
-  let flagLength = 0, flagType = 0, flagValue = 0;
-  if (array1.length !== array2.length) {
-    flagLength = 1;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (typeof (array1[i]) !== typeof (array2[i])) {
-        flagType = 1;
-      } else {
-        if (array1[i] !== array2[i]) {
-          flagValue = 1;
-        }
-      }
-    }
-  }
-  if (flagLength === 1) {
-    return 'flagLength';
-  }
-  if (flagType === 1) {
-    return 'flagType';
-  }
-  if (flagValue === 1) {
-    return 'flagValue';
-  }
-  if (flagLength === 0 && flagType === 0 && flagValue === 0) {
-    return 'Equal';
-  }
-};
-
-//Function to get middle value of an Array using helper funtion middle and eqArrays and log the message
-const assertEqual = function (actual, expected) {
-  let middleArr = [];
-  middleArr = middle(actual);
-  if (eqArrays(middleArr, expected)) {
-    console.log(`✅✅✅Assertion Passed: Middle of Array [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 //Helper function to get the middle value of an input Array
 const middle = function (inpArr) {
   let midElmArr = [];
@@ -61,11 +20,4 @@ const middle = function (inpArr) {
   }
 };
 
-//Test Run
-assertEqual([], []);
-assertEqual([1], []);
-assertEqual([1, 2], []);
-assertEqual([1, 2, 3, 4], [2, 3]);
-assertEqual([1, 2, 3, 4, 5], [3]);
-assertEqual([1, 2, 3, 4, 5, 6], [3, 4]);
-assertEqual([1, 2, 3, 4, 5, 6, 7], [4]);
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = require('../asseerArraysEqual');
+
+const middle = require('../middle');
+
+//Test Run
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
