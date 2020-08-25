59fe5542f2812e509ac071c73a2125c23302a8e8
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-25 02:35:42 +0000

    assertEqual seperate Test function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+//Import Test Exection function from lotide/assertEqual.js
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(1, 1);
+assertEqual(1, 10);
