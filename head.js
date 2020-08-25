c696c53abc1001ca0c3999b04270492f4268ecf1
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-25 02:40:54 +0000

    Refactored with module

--- head.js
+++ head.js
@@ -3,21 +3,5 @@ const head = function (arrIn) {
   return arrIn[0];
 };
 
-//Pass the head variable to the assertEqual function
-const assertEqual = function (head, expected) {
-  if (head === expected) {
-    console.log(`✅✅✅Assertion Passed: "${head}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: "${head}" !== "${expected}"`);
-  }
-};
-
-// Test Run
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(1, 1);
-assertEqual(1, 10);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 5);
-assertEqual(head([]), "Hello");
+//Export to test/headTest.js
+module.exports = head;
